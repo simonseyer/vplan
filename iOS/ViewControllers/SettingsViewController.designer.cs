d7e13c99512a5e00162133ff2a868f9a4ffc8f59
Simon Seyer <simon.seyer@gmail.com>
2017-12-20 00:17:13 +0100

    Project update II

--- iOS/ViewControllers/SettingsViewController.designer.cs
+++ iOS/ViewControllers/SettingsViewController.designer.cs
@@ -1,34 +1,36 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
 using System.CodeDom.Compiler;
 
 namespace FLSVertretungsplan.iOS
 {
-	[Register ("SettingsViewController")]
-	partial class SettingsViewController
-	{
-		[Outlet]
-		UIKit.UICollectionView classCollectionView { get; set; }
+    [Register ("SettingsViewController")]
+    partial class SettingsViewController
+    {
+        [Outlet]
+        UIKit.UICollectionView classCollectionView { get; set; }
 
-		[Outlet]
-		UIKit.UICollectionView schoolCollectionView { get; set; }
-		
-		void ReleaseDesignerOutlets ()
-		{
-			if (schoolCollectionView != null) {
-				schoolCollectionView.Dispose ();
-				schoolCollectionView = null;
-			}
 
-			if (classCollectionView != null) {
-				classCollectionView.Dispose ();
-				classCollectionView = null;
-			}
-		}
-	}
+        [Outlet]
+        UIKit.UICollectionView schoolCollectionView { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (classCollectionView != null) {
+                classCollectionView.Dispose ();
+                classCollectionView = null;
+            }
+
+            if (schoolCollectionView != null) {
+                schoolCollectionView.Dispose ();
+                schoolCollectionView = null;
+            }
+        }
+    }
 }
--- iOS/Views/ChipCell.designer.cs
+++ iOS/Views/ChipCell.designer.cs
@@ -1,26 +1,27 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
 using System.CodeDom.Compiler;
 
 namespace FLSVertretungsplan.iOS
 {
-	[Register ("ChipCell")]
-	partial class ChipCell
-	{
-		[Outlet]
-		public UIKit.UILabel Label { get; set; }
-		
-		void ReleaseDesignerOutlets ()
-		{
-			if (Label != null) {
-				Label.Dispose ();
-				Label = null;
-			}
-		}
-	}
+    [Register ("ChipCell")]
+    partial class ChipCell
+    {
+        [Outlet]
+        public UIKit.UILabel Label { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (Label != null) {
+                Label.Dispose ();
+                Label = null;
+            }
+        }
+    }
 }
