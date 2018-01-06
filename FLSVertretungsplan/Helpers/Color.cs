9c6f245013b7b0d42ac08d5135d08527497ebdde
Simon Seyer <simon.seyer@gmail.com>
2018-01-06 15:05:07 +0100

    Started with defining colors in a central place

---
+++ FLSVertretungsplan/Helpers/Color.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace FLSVertretungsplan
+{
+    public static class Color
+    {
+        public static RgbColor PrimaryTextColor = new RgbColor(23, 43, 76);
+        public static RgbColor SecondaryTextColor = new RgbColor(164, 174, 186);
+    }
+}
