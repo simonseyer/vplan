5af11d6ca454f953c7e82093393b9f457120d326
Simon Seyer <simon.seyer@gmail.com>
2018-01-07 13:14:33 +0100

    Fixed warnings

--- iOS/Views/ChangeTableViewCell.designer.cs
+++ iOS/Views/ChangeTableViewCell.designer.cs
@@ -1,74 +1,81 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
 using System.CodeDom.Compiler;
 
 namespace FLSVertretungsplan.iOS
 {
-	[Register ("ChangeTableViewCell")]
-	partial class ChangeTableViewCell
-	{
-		[Outlet]
-		public UIKit.UILabel ChangeDescriptionLabel { get; private set; }
+    [Register ("ChangeTableViewCell")]
+    partial class ChangeTableViewCell
+    {
+        [Outlet]
+        public UIKit.UILabel ChangeDescriptionLabel { get; private set; }
 
-		[Outlet]
-		public UIKit.UILabel ChangeLabel { get; private set; }
 
-		[Outlet]
-		public UIKit.UIView ContentBackgroundView { get; private set; }
+        [Outlet]
+        public UIKit.UILabel ChangeLabel { get; private set; }
 
-		[Outlet]
-		public UIKit.UILabel HoursLabel { get; private set; }
 
-		[Outlet]
-		public UIKit.UILabel OriginalLessonLabel { get; private set; }
+        [Outlet]
+        public UIKit.UIView ContentBackgroundView { get; private set; }
 
-		[Outlet]
-		public UIKit.UILabel SchoolClassLabel { get; private set; }
 
-		[Outlet]
-		public FLSVertretungsplan.iOS.ChipCell SchoolGradientView { get; private set; }
-		
-		void ReleaseDesignerOutlets ()
-		{
-			if (ChangeDescriptionLabel != null) {
-				ChangeDescriptionLabel.Dispose ();
-				ChangeDescriptionLabel = null;
-			}
+        [Outlet]
+        public UIKit.UILabel HoursLabel { get; private set; }
 
-			if (ChangeLabel != null) {
-				ChangeLabel.Dispose ();
-				ChangeLabel = null;
-			}
 
-			if (ContentBackgroundView != null) {
-				ContentBackgroundView.Dispose ();
-				ContentBackgroundView = null;
-			}
+        [Outlet]
+        public UIKit.UILabel OriginalLessonLabel { get; private set; }
 
-			if (HoursLabel != null) {
-				HoursLabel.Dispose ();
-				HoursLabel = null;
-			}
 
-			if (OriginalLessonLabel != null) {
-				OriginalLessonLabel.Dispose ();
-				OriginalLessonLabel = null;
-			}
+        [Outlet]
+        public UIKit.UILabel SchoolClassLabel { get; private set; }
 
-			if (SchoolClassLabel != null) {
-				SchoolClassLabel.Dispose ();
-				SchoolClassLabel = null;
-			}
 
-			if (SchoolGradientView != null) {
-				SchoolGradientView.Dispose ();
-				SchoolGradientView = null;
-			}
-		}
-	}
+        [Outlet]
+        public FLSVertretungsplan.iOS.ChipCell SchoolGradientView { get; private set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (ChangeDescriptionLabel != null) {
+                ChangeDescriptionLabel.Dispose ();
+                ChangeDescriptionLabel = null;
+            }
+
+            if (ChangeLabel != null) {
+                ChangeLabel.Dispose ();
+                ChangeLabel = null;
+            }
+
+            if (ContentBackgroundView != null) {
+                ContentBackgroundView.Dispose ();
+                ContentBackgroundView = null;
+            }
+
+            if (HoursLabel != null) {
+                HoursLabel.Dispose ();
+                HoursLabel = null;
+            }
+
+            if (OriginalLessonLabel != null) {
+                OriginalLessonLabel.Dispose ();
+                OriginalLessonLabel = null;
+            }
+
+            if (SchoolClassLabel != null) {
+                SchoolClassLabel.Dispose ();
+                SchoolClassLabel = null;
+            }
+
+            if (SchoolGradientView != null) {
+                SchoolGradientView.Dispose ();
+                SchoolGradientView = null;
+            }
+        }
+    }
 }
