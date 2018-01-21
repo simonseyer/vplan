58a57154a4ad68af92754f8b961665cb186febe1
Simon Seyer <simon.seyer@gmail.com>
2018-01-21 17:50:11 +0100

    Fixes crash #6 — Sharing violation

--- FLSVertretungsplan/Plugins/Persistence/JsonVplanPersistence.cs
+++ FLSVertretungsplan/Plugins/Persistence/JsonVplanPersistence.cs
@@ -4,21 +4,30 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.IO;
+using System.Threading;
 
 namespace FLSVertretungsplan
 {
     public class JsonVplanPersistence: IVplanPersistence
     {
-        static string newClassesFileName = "newSchoolClassBookmarks.json";
-        static string schoolBookmarksFileName = "schoolBookmarks.json";
-        static string schoolClassBookmarksFileName = "schoolClassBookmarks.json";
-        static string vplanFileName = "vplan.json";
+        static string NewClassesFileName = "newSchoolClassBookmarks.json";
+        static string SchoolBookmarksFileName = "schoolBookmarks.json";
+        static string SchoolClassBookmarksFileName = "schoolClassBookmarks.json";
+        static string VplanFileName = "vplan.json";
+
+        // Prevent loading and persisting the json files at the same time by
+        // locking access to the files. Sharing violation could be raised otherwise.
+        SemaphoreSlim NewSchoolClassBookmarksSempahore = new SemaphoreSlim(1, 1);
+        SemaphoreSlim SchoolBookmarksSempahore = new SemaphoreSlim(1, 1);
+        SemaphoreSlim SchoolClassBookmarksSempahore = new SemaphoreSlim(1, 1);
+        SemaphoreSlim VplanSempahore = new SemaphoreSlim(1, 1);
 
         public async Task<List<SchoolClassBookmark>> LoadNewSchoolClassBookmarks()
         {
             try
             {
-                return await Load<List<SchoolClassBookmark>>(newClassesFileName) ?? new List<SchoolClassBookmark>();
+                var result = await Load<List<SchoolClassBookmark>>(NewClassesFileName, NewSchoolClassBookmarksSempahore);
+                return result ?? new List<SchoolClassBookmark>();
             }
             catch (Exception ex)
             {
@@ -31,7 +40,8 @@ namespace FLSVertretungsplan
         {
             try
             {
-                return await Load<List<SchoolBookmark>>(schoolBookmarksFileName) ?? new List<SchoolBookmark>();
+                var result = await Load<List<SchoolBookmark>>(SchoolBookmarksFileName, SchoolBookmarksSempahore);
+                return result ?? new List<SchoolBookmark>();
             }
             catch (Exception ex)
             {
@@ -44,7 +54,8 @@ namespace FLSVertretungsplan
         {
             try
             {
-                return await Load<List<SchoolClassBookmark>>(schoolClassBookmarksFileName) ?? new List<SchoolClassBookmark>();
+                var result = await Load<List<SchoolClassBookmark>>(SchoolClassBookmarksFileName, SchoolClassBookmarksSempahore);
+                return result ?? new List<SchoolClassBookmark>();
             }
             catch (Exception ex)
             {
@@ -57,7 +68,7 @@ namespace FLSVertretungsplan
         {
             try
             {
-                return await Load<Vplan>(vplanFileName);
+                return await Load<Vplan>(VplanFileName, VplanSempahore);
             }
             catch (Exception ex)
             {
@@ -66,49 +77,71 @@ namespace FLSVertretungsplan
             }
         }
 
+
         public async Task PersistNewSchoolClassBookmarks(List<SchoolClassBookmark> newSchoolClasses)
         {
-            await Persist(newSchoolClasses, newClassesFileName);
+            await Persist(newSchoolClasses, NewClassesFileName, NewSchoolClassBookmarksSempahore);
         }
 
         public async Task PersistSchoolBookmarks(List<SchoolBookmark> schoolBookmarks)
         {
-            await Persist(schoolBookmarks, schoolBookmarksFileName);
+            await Persist(schoolBookmarks, SchoolBookmarksFileName, SchoolBookmarksSempahore);
         }
 
         public async Task PersistSchoolClassBookmarks(List<SchoolClassBookmark> schoolClassBookmarks)
         {
-            await Persist(schoolClassBookmarks, schoolClassBookmarksFileName);
+            await Persist(schoolClassBookmarks, SchoolClassBookmarksFileName, SchoolClassBookmarksSempahore);
         }
 
         public async Task PersistVplan(Vplan vplan)
         {
-            await Persist(vplan, vplanFileName);
+            await Persist(vplan, VplanFileName, VplanSempahore);
         }
 
-        private async Task Persist(object theObject, string fileName)
+
+        async Task Persist(object theObject, string fileName, SemaphoreSlim semaphore)
         {
-            var json = await Task.Run(() => JsonConvert.SerializeObject(theObject));
-            var filePath = CreatePersistentPath(fileName);
-            using (var file = File.Open(filePath, FileMode.Create, FileAccess.Write))
-            using (var strm = new StreamWriter(file))
+            await semaphore.WaitAsync();
+            try
             {
-                await strm.WriteAsync(json);
+                var json = await Task.Run(() => JsonConvert.SerializeObject(theObject));
+                var filePath = CreatePersistentPath(fileName);
+                using (var file = File.Open(filePath, FileMode.Create, FileAccess.Write))
+                using (var strm = new StreamWriter(file))
+                {
+                    await strm.WriteAsync(json);
+                }
+            } catch {
+                throw;
+            } finally {
+                semaphore.Release();
             }
         }
 
-        private async Task<T> Load<T>(string fileName)
+        async Task<T> Load<T>(string fileName, SemaphoreSlim semaphore)
         {
-            var filePath = CreatePersistentPath(fileName);
-            using (var file = File.Open(filePath, FileMode.Open, FileAccess.Read))
-            using (var strm = new StreamReader(file))
+            await semaphore.WaitAsync();
+            try
             {
-                var json = await strm.ReadToEndAsync();
-                return await Task.Run(() => JsonConvert.DeserializeObject<T>(json));
+                var filePath = CreatePersistentPath(fileName);
+                using (var file = File.Open(filePath, FileMode.Open, FileAccess.Read))
+                using (var strm = new StreamReader(file))
+                {
+                    var json = await strm.ReadToEndAsync();
+                    return await Task.Run(() => JsonConvert.DeserializeObject<T>(json));
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                semaphore.Release();
             }
         }
 
-        private string CreatePersistentPath(string fileName)
+        string CreatePersistentPath(string fileName)
         {
             string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             return Path.Combine(path, fileName);
