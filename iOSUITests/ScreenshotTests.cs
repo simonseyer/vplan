5414cb0e2d9b466d67a3c108afae3ef98dd28e6f
Simon Seyer <simon.seyer@gmail.com>
2018-03-11 15:47:22 +0100

    Restructured and improved iOS UI tests

--- iOSUITests/ScreenshotTests.cs
+++ iOSUITests/ScreenshotTests.cs
@@ -3,21 +3,19 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using NUnit.Framework;
 using Xamarin.UITest;
 using Xamarin.UITest.iOS;
-using Xamarin.UITest.Queries;
 
 namespace iOS.uitest
 {
     [TestFixture]
     public class ScreenshotTests
     {
-        iOSApp app;
-
         static ScreenshotTests()
         {
-            LoadAllDevices();
+            LoadAllDevices(OSVersion);
         }
 
         [SetUp]
@@ -26,16 +24,140 @@ namespace iOS.uitest
             
         }
 
-        static string[] Devices = new string[]
+        static readonly string[] Devices = new string[]
         {
-            "iPhone X (11.2)",
-            "iPhone 8 Plus (11.2)",
-            "iPhone 8 (11.2)",
-            "iPhone 5s (11.2)"
+            "iPhone X",
+            "iPhone 8 Plus",
+            "iPhone 8",
+            "iPhone 5s"
         };
+        static readonly string OSVersion = "11.2";
+        static readonly string BundlePath = "../../../iOS/bin/iPhoneSimulator/Debug/FLSVertretungsplan.iOS.app";
+        static readonly string TargetPath = "../../../iOS/fastlane/screenshots/de-DE/";
+
+        [Test]
+        public void ProcessAllDevices()
+        {
+            foreach (string device in Devices)
+            {
+                iOSApp app = PrepareDevice(device);
+                TestApp(app);
+                CollectScreenshots(device);
+            }
+        }
+
+        static void TestApp(iOSApp app)
+        {
+            app.WaitForElement(c => c.Marked("Auf geht's"));
+            app.Tap(c => c.Marked("Auf geht's"));
+
+            app.Screenshot("no-changes");
+
+            app.Tap(c => c.Marked("Filter"));
+            app.Tap(c => c.Marked("BFS"));
+            app.Tap(c => c.Marked("BG"));
+            app.Tap(c => c.Marked("HBFS"));
+
+            app.Tap(c => c.Marked("1082"));
+            app.Tap(c => c.Marked("11/12 M"));
+            app.Tap(c => c.Marked("11/11 G"));
+
+            app.ScrollDown("SchoolClassCollectionView", strategy: ScrollStrategy.Gesture);
+
+            app.Tap(c => c.Marked("12"));
+            app.Tap(c => c.Marked("1292"));
+
+            app.ScrollUp("SchoolClassCollectionView", strategy: ScrollStrategy.Gesture);
+
+            app.Screenshot("filter");
+
+            app.Tap(c => c.Marked("Mein Plan"));
+
+            app.Screenshot("my-plan");
+
+            app.Tap(c => c.Marked("Schul-Plan"));
+            app.SwipeRightToLeft();
+
+            app.Screenshot("school-plan");
+
+            app.TouchAndHold("Deutsch bei P. Pohl in Raum 301");
+
+            app.Screenshot("sharing");
+        }
+
+        static iOSApp PrepareDevice(string device)
+        {
+            Assert.NotNull(AllDevices[device], "Device " + device + " not found");
+
+            iOSApp app = ConfigureApp
+                .iOS
+                .AppBundle(BundlePath)
+                .DeviceIdentifier(AllDevices[device])
+                .EnableLocalScreenshots()
+                .StartApp();
+            
+            app.Invoke("activateTestEnvironment:", "true");
+
+            return app;
+        }
+
+        static void CollectScreenshots(string device)
+        {
+            Directory.CreateDirectory(TargetPath);
+
+            var deviceName = device.Replace(' ', '-');
+            for (var i = 1; i <= 5; i++)
+            {
+                try
+                {
+                    var oldPath = string.Format("screenshot-{0}.png", i);
+                    var newPath = string.Format("{0}/{1}-{2}.png", TargetPath, deviceName, i);
+                    try
+                    {
+                        File.Delete(newPath);
+                    }
+                    catch (IOException) { }
+                    File.Move(oldPath, newPath);
+                }
+                catch (Exception e)
+                {
+                    Debug.Print(e.Message);
+                }
+            }
+        }
 
         static Dictionary<string, string> AllDevices;
-        static void LoadAllDevices()
+        static void LoadAllDevices(string osVersion)
+        {
+            /* Matches:
+             * iPhone 8 Plus (11.2) [06E55EA2-AE29-4D72-83DD-122931620569] (Simulator)
+             * -------------  ----   ------------------------------------
+             * 1              2      3
+             * 
+             * Doesn't match:
+             * iPhone 8 Plus (11.2) + Apple Watch Series 3 - 42mm (4.2) [35BEAFCB-78E6-4067-B551-C5897F8B5644] (Simulator)
+             */
+            var pattern = @"^([\w\s]*)\s\(([0-9.]+)\)\s\[([\w-]*)\]";
+            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            var devices = new Dictionary<string, string>();
+            foreach (string device in LoadDeviceList())
+            {
+                Match match = regex.Match(device);
+                if (!match.Success || match.Groups[2].Value != osVersion)
+                {
+                    continue;
+                }
+
+                string name = match.Groups[1].Value.Trim();
+                string id = match.Groups[3].Value;
+                devices[name] = id;
+            }
+
+            AllDevices = devices;
+        }
+
+        static string[] LoadDeviceList()
         {
             var proc = new Process();
             proc.StartInfo.FileName = @"xcrun";
@@ -47,96 +169,9 @@ namespace iOS.uitest
             var output = proc.StandardOutput.ReadToEnd();
 
             proc.WaitForExit();
-            var exitCode = proc.ExitCode;
             proc.Close();
 
-            var devices = new Dictionary<string, string>();
-            foreach (string device in output.Split("\n".ToCharArray()))
-            {
-                var components = device.Split('[');
-                if (components.Count() != 2)
-                {
-                    continue;
-                }
-                var name = components[0].Trim();
-                var id = components[1].Split(']')[0];
-                devices[name] = id;
-            }
-
-            AllDevices = devices;
-        }
-
-        [Test]
-        public void AppLaunches()
-        {
-            foreach (string device in Devices)
-            {
-                app = ConfigureApp
-                .iOS
-                .AppBundle("../../../iOS/bin/iPhoneSimulator/Debug/FLSVertretungsplan.iOS.app")
-                .DeviceIdentifier(AllDevices[device])
-                .EnableLocalScreenshots()
-                .StartApp();
-                app.Invoke("activateTestEnvironment:", "true");
-
-                app.WaitForElement(c => c.Marked("Auf geht's"));
-                app.Tap(c => c.Marked("Auf geht's"));
-
-                app.Screenshot("no-changes");
-
-                app.Tap(c => c.Marked("Filter"));
-                app.Tap(c => c.Marked("BFS"));
-                app.Tap(c => c.Marked("BG"));
-                app.Tap(c => c.Marked("HBFS"));
-
-                app.Tap(c => c.Marked("1082"));
-                app.Tap(c => c.Marked("11/4 W"));
-                app.Tap(c => c.Marked("11/11 G"));
-                app.ScrollDown("SchoolClassCollectionView");
-                app.Tap(c => c.Marked("12"));
-                app.Tap(c => c.Marked("1292"));
-
-                app.Screenshot("filter");
-
-                app.Tap(c => c.Marked("Mein Plan"));
-
-                app.Screenshot("my-plan");
-
-                app.Tap(c => c.Marked("Schul-Plan"));
-                app.SwipeRightToLeft();
-
-                app.Screenshot("school-plan");
-
-                app.TouchAndHold("Deutsch bei P. Pohl in Raum 301");
-
-                app.Screenshot("sharing");
-
-                var path = "../../../iOS/metadata/screenshots/de-DE/" + device;
-                try
-                {
-                    Directory.Delete(path);
-                } 
-                catch (IOException) { }
-                Directory.CreateDirectory(path);
-                for (var i = 1; i <= 5; i++)
-                {
-                    try
-                    {
-                        var oldPath = string.Format("screenshot-{0}.png", i);
-                        var newPath = string.Format("{0}/screenshot-{1}.png", path, i);
-                        try
-                        {
-                            File.Delete(newPath);
-                        }
-                        catch (IOException) {}
-                        File.Move(oldPath, newPath);
-                    }
-                    catch (Exception e)
-                    {
-                        Debug.Print(e.Message);
-                    }
-                }
-            }
+            return output.Split(Environment.NewLine.ToCharArray());
         }
     }
 }
